e469be7c5328146604955087d84614c0888ffbc9
Hashane <jhashane@gmail.com>
2026-08-09 04:21:25 +0300

    enhancement: display image and use HTML function

--- lib/screens/page_screen.dart
+++ lib/screens/page_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cms/core/providers/cms_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PageScreen extends ConsumerWidget {
@@ -13,16 +14,27 @@ class PageScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Page Screen')),
       body: pageAsyncValue.when(
-        data: (page) => SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Title: ${page.title}'),
-              Text('Content: ${page.bodyHtml}'),
-            ],
-          ),
-        ),
+        data: (page) {
+          debugPrint(page.coverImageUrl);
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (page.coverImageUrl != null) ...[
+                  Image.network(
+                    page.coverImageUrl!,
+                    errorBuilder: (context, error, stackTrace) =>
+                        const SizedBox.shrink(),
+                  ),
+                  const SizedBox(height: 12),
+                ],
+                Text('Title: ${page.title}'),
+                Html(data: page.bodyHtml),
+              ],
+            ),
+          );
+        },
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (err, stack) => Center(child: Text('Failed to load menu: $err')),
       ),
