9c7519b50e4a4fd309fe3f19333aad5c6e408bc6
Hashane <jhashane@gmail.com>
2026-08-09 04:08:39 +0300

    enhancement: show MenuScreen directly.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cms/screens/menu_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,11 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('CMS')),
-        body: Center(child: Text('Hello World!')),
-      ),
-    );
+    return MaterialApp(home: const MenuScreen());
   }
 }
