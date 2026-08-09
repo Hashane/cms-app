c7274445ead3b64a29d937891f2130f68aa4df92
Hashane <jhashane@gmail.com>
2026-08-09 04:21:47 +0300

    refactor: updated url with tunnel url.

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 final dio = Dio(
   BaseOptions(
-    baseUrl: 'https://cms.test/api/v1',
+    baseUrl: 'https://plgj4xmyfr.sharedwithexpose.com/api/v1',
     headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
   ),
 );
